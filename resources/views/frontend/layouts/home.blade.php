fa70af6f3021d44b8d326663c2f915af1da73ac2
sulistyawati11 <e41192287@student.polije.ac.id>
2021-07-17 05:28:22 +0700

    tampilan

--- resources/views/frontend/layouts/home.blade.php
+++ resources/views/frontend/layouts/home.blade.php
@@ -24,7 +24,7 @@
 
         <div class="row">
           <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-            <img src="{{ asset('frontend/assets/img/about.jpg')}}" class="img-fluid" alt="">
+            <img src="{{ asset('frontend/assets/img/abouts.png')}}" class="img-fluid" alt="">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
             <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
